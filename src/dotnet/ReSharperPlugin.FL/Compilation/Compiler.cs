f3677f894b6f69abd4a96c4c7278d1f67bef06a5
AliNoorkazemi <alinoorkazemi@gmail.com>
2024-01-18 18:54:25 +0330

    Add compilation and instrumentation logic

---
+++ src/dotnet/ReSharperPlugin.FL/Compilation/Compiler.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using ReSharperPlugin.FL.Instrumentation;
+
+namespace ReSharperPlugin.FL.Compilation;
+
+public class Compiler : IDisposable
+{
+    private const string MainMethodName = "Main";
+    
+    private string MainClassName { get; set; }
+    
+    private object?[] MainMethodArgs { get; set; }
+    
+    private const string AssemblyName = "FakeAssembly";
+
+    private readonly MemoryStream _memoryStream;
+
+    public Compiler()
+    {
+        _memoryStream = new MemoryStream();
+    }
+
+    public EmitResult Compile(string codeContent)
+    {
+        codeContent = SetMainMethodAsPublic(codeContent);
+        
+        var instrumentedCode = InstrumentFactory.ReturnInstrumentedCode(codeContent);
+
+        var realNamespace = GetNameSpace(codeContent);
+
+        MainClassName = realNamespace is not null ? $"{realNamespace}.Program" : "Program";
+
+        MainMethodArgs = GetMainMethodArgs(codeContent);
+
+        var syntaxTree = SyntaxFactory.ParseSyntaxTree(instrumentedCode);
+        var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+
+        var compilation = CSharpCompilation.Create(AssemblyName)
+            .WithOptions(compilationOptions)
+            .AddReferences(GetMetadataReferences())
+            .AddSyntaxTrees(syntaxTree);
+        
+        return compilation.Emit(_memoryStream);
+    }
+
+    private static string SetMainMethodAsPublic(string codeContent)
+    {
+        return codeContent.Contains("public static void Main")
+            ? codeContent
+            : codeContent.Replace("static void Main", "public static void Main");
+    }
+
+    private static object?[] GetMainMethodArgs(string codeContent)
+    {
+        return codeContent.Contains("static void Main()")
+            ? Array.Empty<object?>()
+            : new object?[] { Array.Empty<string>() };
+    }
+
+    private static string? GetNameSpace(string originalCode)
+    {
+        var lines = originalCode.Split('\r').ToList();
+
+        var namespaceLine = lines.FirstOrDefault(l => l.Contains("namespace"));
+
+        return namespaceLine?.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ElementAt(1);
+    }
+
+    private static MetadataReference[] GetMetadataReferences()
+    {
+        return new MetadataReference[]
+        {
+            MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+            MetadataReference.CreateFromFile(typeof(Console).Assembly.Location),
+            MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location),
+            MetadataReference.CreateFromFile(
+                typeof(System.Runtime.AssemblyTargetedPatchBandAttribute).Assembly.Location),
+            MetadataReference.CreateFromFile(Assembly.Load("System.Runtime").Location),
+            MetadataReference.CreateFromFile(Assembly.Load("System.Text.RegularExpressions").Location),
+        };
+    }
+
+    public bool Run(string input)
+    {
+        try
+        {
+            _memoryStream.Seek(0, SeekOrigin.Begin);
+
+            var assembly = Assembly.Load(_memoryStream.ToArray());
+
+            var libraryClassType = assembly.GetType(MainClassName);
+            var libraryInstance = Activator.CreateInstance(libraryClassType!);
+            var libraryMethod = libraryClassType!.GetMethod(MainMethodName);
+
+            using var stringReader = new StringReader(input);
+        
+            Console.SetIn(stringReader);
+
+            try
+            {
+                libraryMethod!.Invoke(libraryInstance, MainMethodArgs);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        catch (Exception e)
+        {
+            var t = 2;
+
+            return false;
+        }
+    }
+
+    public void Dispose()
+    {
+        _memoryStream.Dispose();
+    }
+}
---
+++ src/dotnet/ReSharperPlugin.FL/Instrumentation/InstrumentFactory.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ReSharperPlugin.FL.Instrumentation;
+
+public static class InstrumentFactory
+{
+    private const string InstrumentCode = "db24733c-5d52-4aad-bd7e-92d4cde5ff5b";
+
+    public static string ReturnInstrumentedCode(string originalCode)
+    {
+        var originalLines = originalCode.Split('\r').ToList();
+        var newLines = new List<string>(originalLines.Count);
+
+        for (var i = 0; i < originalLines.Count ; i++)
+        {
+            var originalLine = originalLines.ElementAt(i);
+
+            if (ContainsNoneInstrumentedWords(originalLine) ||
+                InvalidLines().Match(originalLine).Success ||
+                IsComment(originalLine) ||
+                (
+                    !IsControlFlowAction(originalLine) &&
+                    !IsEndWithSemicolon(originalLine)
+                )
+               )
+            {
+                newLines.Add(originalLine);
+                continue;
+            }
+
+            var newLine = AppendInstrumentationAtStart(originalLine, i + 1);
+            
+            newLines.Add(newLine);
+        }
+
+        return AppendAllLinesTogether(newLines);
+    }
+
+    private static bool ContainsNoneInstrumentedWords(string line)
+    {
+        return line.TrimStart('\n').TrimStart('\r').Split(' ').Any(word => NoneInstrumentedWords.Contains(word));
+    }
+
+    private static IEnumerable<string> NoneInstrumentedWords => new[]
+    {
+        "using", "public", "static", "private", "protected", "internal", "class", "interface", "try", "catch", "const",
+        "finally", "do", "struct", "#region", "#endregion", "enum", "else"
+    };
+
+    private static bool IsEndWithSemicolon(string originalLine)
+    {
+        return originalLine.TrimEnd(' ').TrimEnd('\r').EndsWith(";");
+    }
+
+    private static bool IsControlFlowAction(string originalLine)
+    {
+        return ControlFlowActions.Any(c => originalLine.TrimStart(' ').StartsWith(c));
+    }
+
+    private static IEnumerable<string> ControlFlowActions => new[]
+    {
+        "for", "if", "foreach", "while"
+    };
+
+    private static bool IsComment(string originalLine)
+    {
+        return originalLine.TrimStart(' ').StartsWith("//");
+    }
+
+    private static string AppendInstrumentationAtStart(string originalLine, int lineNumber)
+    {
+        return $"Console.WriteLine($\"\\n{lineNumber}#{InstrumentCode}\"); {originalLine}";
+    }
+
+    private static string AppendAllLinesTogether(IEnumerable<string> lines)
+    {
+        return string.Join("\n", lines);
+    }
+
+    private static Regex InvalidLines()
+    {
+        return new Regex("^[\\s\\{\\}\\(\\)\\;\\,]+$");
+    }
+
+    public static IReadOnlyCollection<string> GetExecutedLines(string outputConsole)
+    {
+        return outputConsole.Split('\n')
+            .Where(line => line.Contains(InstrumentCode))
+            .Select(line => line.Split('#')[0])
+            .Distinct()
+            .ToList();
+    }
+
+    public static string GetActualOutput(string outputConsole)
+    {
+        var filteredStrings = outputConsole.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries)
+            .Where(line => !line.Contains(InstrumentCode))
+            .ToArray();
+
+        return string.Join("", filteredStrings);
+    }
+}
