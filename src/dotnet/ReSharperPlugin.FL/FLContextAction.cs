618ebbead5a361804baf476a4b23413b6441ce32
AliNoorkazemi <alinoorkazemi@gmail.com>
2024-01-18 14:46:06 +0330

    Handle simple input action

--- src/dotnet/ReSharperPlugin.FL/FLContextAction.cs
+++ src/dotnet/ReSharperPlugin.FL/FLContextAction.cs
@@ -1,8 +1,14 @@
 using System;
 using JetBrains.Application.Progress;
+using JetBrains.IDE.UI;
+using JetBrains.IDE.UI.Extensions;
+using JetBrains.Lifetimes;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.ContextActions;
 using JetBrains.ReSharper.Feature.Services.CSharp.ContextActions;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Resources.Shell;
+using JetBrains.Rider.Model.UIAutomation;
 using JetBrains.TextControl;
 using JetBrains.Util;
 
@@ -15,17 +21,49 @@ namespace ReSharperPlugin.FL;
     Priority = -10)]
 public class FLContextAction : ContextActionBase
 {
+    private readonly ICSharpContextActionDataProvider _provider;
+
+    public FLContextAction(ICSharpContextActionDataProvider provider)
+    {
+        _provider = provider;
+    }
+
     protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
     {
+        ICSharpTypeDeclaration typeDeclaration = _provider.GetSelectedElement<ICSharpTypeDeclaration>();
+        if (typeDeclaration is null)
+            return null;
+        
+        var dialogHost = solution.GetComponent<IDialogHost>();
+
+        BeTextBox textBox;
+
+        dialogHost.Show(
+            getDialog: lt => BeControls.GetDialog(
+                    dialogContent: textBox = BeControls.GetTextBox(lt),
+                    title: "title",
+                    id: "Fl-id")
+                .WithOkButton(lt, () => MessageBox.ShowInfo($"Okay {textBox.GetText()}"))
+                .WithCancelButton(lt),
+            parentLifetime: Lifetime.Eternal);
+
         return textControl =>
         {
-            Console.WriteLine("hello");
+            int caretOffset = _provider.CaretOffset;
+            string generatedGuid = Guid.NewGuid().ToString().ToUpper();
+            string textToInsert = $"\"{generatedGuid}\";";
+        
+            using (WriteLockCookie.Create())
+            {
+                textControl.Document.InsertText(caretOffset, textToInsert);
+                textControl.Caret.MoveTo(caretOffset + textToInsert.Length, CaretVisualPlacement.DontScrollIfVisible);
+            }
         };
     }
 
     public override string Text => "FL";
     public override bool IsAvailable(IUserDataHolder cache)
     {
-        return true;
+        return _provider.SelectedElement is not null;
     }
 }
